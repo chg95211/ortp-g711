45102b6d1af6c94fdf987300f4c2dfb20924ef27
yqwung <1522943999@qq.com>
2018-05-25 14:22:46 +0800

    modify usage

--- ortp_send_g711.c
+++ ortp_send_g711.c
@@ -34,7 +34,7 @@ void stophandler(int signum)
     runcond=0;
 }
 
-static const char *help="usage: rtpsend	filename dest_ip4addr dest_port [ --with-clockslide <value> ] [ --with-jitter <milliseconds>]\n";
+static const char *help="usage: ortp_send_g711 filename dest_ip4addr dest_port [ --with-clockslide <value> ] [ --with-jitter <milliseconds>]\n";
 
 int main(int argc, char *argv[])
 {
